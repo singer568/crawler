fce1afc310923e8d44c0a866103fb9e0f11c11dc
singer568 <singer568@163.com>
2014-11-18 18:50:05 +0800

    重构为maven工程

---
+++ login/src/org/javacoo/cowswing/main/NetProxySettingDialog.java
@@ -0,0 +1,146 @@
+/**
+ * 如石子一粒,仰高山之巍峨,但不自惭形秽.
+ * 若小草一棵,慕白杨之伟岸,却不妄自菲薄.
+ */
+package org.javacoo.cowswing.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JComponent;
+
+import org.apache.commons.lang.StringUtils;
+import org.javacoo.cowswing.base.constant.Constant;
+import org.javacoo.cowswing.core.cache.UserCacheManager;
+import org.javacoo.cowswing.core.event.CowSwingListener;
+import org.javacoo.cowswing.ui.view.dialog.AbstractDialog;
+
+/**
+ * 网络代理设置
+ * 
+ * <p>说明:</p>
+ * <li></li>
+ * @author DuanYong
+ * @since 2013-11-3 上午9:50:23
+ * @version 1.0
+ */
+public class NetProxySettingDialog extends AbstractDialog implements
+		CowSwingListener {
+	private static final long serialVersionUID = 1L;
+	private static NetProxySettingDialog instance;
+	private NetProxySettingBean netSettingBean;
+	/** 用户缓存管理类 */
+	protected UserCacheManager userCacheManager = UserCacheManager.getInstance();
+	private NetProxySettingDialog(){
+		super(300,280,true);
+		this.setUndecorated(true);
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e) {
+				checkValue();
+				save();
+			}
+		});
+		getFinishButton().setText("保存");
+		getCancelButton().setText("取消");
+	}
+	
+	/**
+	 * @return the instance
+	 */
+	public static NetProxySettingDialog getInstance() {
+		if(null == instance){
+			instance = new NetProxySettingDialog();
+		}
+		return instance;
+	}
+
+	@Override
+	public JComponent getCenterPane() {
+		if (centerPane == null) {
+			centerPane = NetProxySettingPanel.getInstance();
+		}
+		return centerPane;
+	}
+	
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.javacoo.cowswing.ui.view.dialog.AbstractDialog#
+	 * finishButtonActionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	protected void finishButtonActionPerformed(ActionEvent event) {
+		if(checkValue()){
+			save();
+			this.dispose();
+		}
+	}
+	/**
+	 * 校验参数
+	 * <p>方法说明:</>
+	 * <li></li>
+	 * @author DuanYong
+	 * @since 2013-9-12 下午2:20:30
+	 * @version 1.0
+	 * @exception 
+	 * @return
+	 */
+	private boolean checkValue(){
+		netSettingBean = NetProxySettingPanel.getInstance().getData();
+		
+		return true;
+	}	
+	/**
+	 * 建立连接
+	 * <p>方法说明:</>
+	 * <li></li>
+	 * @author DuanYong
+	 * @since 2013-9-12 下午2:37:59
+	 * @version 1.0
+	 * @exception
+	 */
+	private void save(){
+		userCacheManager.setValue(Constant.NET_PROXY_SETTING_HOST,netSettingBean.getProxyHost());
+		userCacheManager.setValue(Constant.NET_PROXY_SETTING_PORT,netSettingBean.getProxyPort());
+		userCacheManager.setValue(Constant.NET_PROXY_SETTING_USERNAME,netSettingBean.getUserName());
+		userCacheManager.setValue(Constant.NET_PROXY_SETTING_PASSWORD,netSettingBean.getPassword());
+		userCacheManager.setValue(Constant.NET_PROXY_SETTING_DOMAIN,netSettingBean.getDomain());
+		userCacheManager.setValue(Constant.NET_PROXY_SETTING_ISUSED,netSettingBean.getIsUsed());
+	}
+	protected void initData(String type) {
+		netSettingBean = new NetProxySettingBean();
+		netSettingBean.setProxyHost(userCacheManager.getValue(Constant.NET_PROXY_SETTING_HOST));
+		netSettingBean.setProxyPort(userCacheManager.getValue(Constant.NET_PROXY_SETTING_PORT));
+		netSettingBean.setUserName(userCacheManager.getValue(Constant.NET_PROXY_SETTING_USERNAME));
+		netSettingBean.setPassword(userCacheManager.getValue(Constant.NET_PROXY_SETTING_PASSWORD));
+		netSettingBean.setDomain(userCacheManager.getValue(Constant.NET_PROXY_SETTING_DOMAIN));
+		if (StringUtils.isNotBlank(userCacheManager.getValue(Constant.NET_PROXY_SETTING_ISUSED))) {
+			netSettingBean.setIsUsed(userCacheManager.getValue(Constant.NET_PROXY_SETTING_ISUSED));
+		} else {
+			netSettingBean.setIsUsed("false");
+		}
+		fillJTabbedPane();
+	}
+	public void dispose(){
+		super.dispose();
+		centerPane = null;
+	}
+	/**
+	 * 填充JTabbedPane值
+	 * <p>方法说明:</p>
+	 * @auther DuanYong
+	 * @since 2012-12-3 下午12:20:32
+	 * @return void
+	 */
+	private void fillJTabbedPane(){
+		logger.info("网络代理连接参数设置");
+		NetProxySettingPanel.getInstance().initData(netSettingBean);
+	}
+	
+	
+
+	
+}
---
+++ login/src/org/javacoo/cowswing/main/Splash.java
@@ -0,0 +1,25 @@
+/**
+ * 如石子一粒,仰高山之巍峨,但不自惭形秽.
+ * 若小草一棵,慕白杨之伟岸,却不妄自菲薄.
+ */
+package org.javacoo.cowswing.main;
+
+/**
+ * 
+ * <p>说明:</p>
+ * <li></li>
+ * @author DuanYong
+ * @since 2013-10-8 上午10:01:51
+ * @version 1.0
+ */
+public interface Splash {
+	public void splashOn();
+	
+	public void splashOff();
+	
+	public void increaseProgress(int value);
+	
+	public void increaseProgress(int value, String message);
+	
+	public void resetProgress();
+}
